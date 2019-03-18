01e38e735017aa1fa77c899b3edeed2f1c02ee76
League <league@WTS9.attlocal.net>
2019-03-18 16:53:17 -0700

    I started making a change calculator.

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,13 +15,15 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String n=JOptionPane.showInputDialog("How many nickles do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+Integer.parseInt(n);
 		// Ask the user how many dimes they have, and convert their answer
-
+String d=JOptionPane.showInputDialog("How many dimes do you have?");
+Integer.parseInt(d);
 		// Ask the user how many quarters they have, and convert their answer
-
+String q=JOptionPane.showInputDialog("How man quarters do you have?");
+Integer.parseInt(q);
 		// Calculate how much money the user has and save it in a double variable 
 
 		// Tell the user how much money they have
