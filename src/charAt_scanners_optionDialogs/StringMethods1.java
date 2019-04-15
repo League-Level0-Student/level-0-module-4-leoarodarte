8193bd6a93faeaf9a0f0e5aa2ba35b8f3c18f172
League <league@WTS9.attlocal.net>
2019-04-15 16:58:09 -0700

    cfhxf

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -6,26 +6,39 @@ package charAt_scanners_optionDialogs;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+	
 		// 1. Create a String variable and initialize it to whatever you want
-
+String s= "hello";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(s.charAt(s.length()-1));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
 
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
+for(int i=0; i<s.length(); i++) {
+	System.out.println(s.charAt(i));
 
-		
-		// 5. Pick a char inside your String, and use a loop to determine
+
+if(s.charAt(i)== 'e') {
+	multiply();
+	
+}
+	
+	
+
+
+}
+}
+	}	
+	public static void multiply(int a, int b) {
+		System.out.println(a*b);
+}// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
-	}
-}
+
 
 
