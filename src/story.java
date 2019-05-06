4c5a04a28f0ceab821f329f8b4e7fc6d9d166ff1
League <league@WTS9.attlocal.net>
2019-05-06 16:50:51 -0700

    Create story.java

---
+++ src/story.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class story {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"Once upon a time, there was a game called fortnite. It was made by"
+			+ "Epic Games and was very popular."
+			+ "One day, tons of fans walked up to the main facility."
+			+ "We don't want planes anymore! They suck!"
+			+ ""
+			+ "Bring back drum guns and burst assualt rifles!");
+	JOptionPane.showOptionDialog(null, "Epic games decided to...", title, optionType, messageType, icon, options, initialValue)
+}
+}
